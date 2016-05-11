e63d6ee37ba7ae41430cf5def1dcb7473f9d8449
lanou3g <1069239913@qq.com>
2016-05-11 20:19:50 +0800

    组员第一次提交

--- testGit/ViewController.m
+++ testGit/ViewController.m
@@ -16,9 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"老板威武");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
